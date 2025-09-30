399c2b4157234e187fff796dfc8da8c68915e6b7
Lars Hennig <lars@bespin-studios.com>
2025-09-30 09:48:19 +0200

    feat: add event enum

---
+++ src/byteShard/Enum/Event.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace byteShard\Enum;
+
+enum Event
+{
+    case OnBlur;
+    case OnChange;
+    case OnCheck;
+    case OnClick;
+    case OnDoubleClick;
+    case OnDrop;
+    case OnEnter;
+    case OnInputChange;
+    case OnLinkClick;
+    case OnPoll;
+    case OnSelect;
+    case OnSelectOption;
+    case OnStateChange;
+    case OnTabClick;
+    case OnTabClose;
+    case OnUncheck;
+    case OnUpdateFile;
+    case OnUploadFile;
+    case OnValueChange;
+}
